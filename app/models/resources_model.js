e9a5b8c9e257bf3ff36b2cfc55a2f6fec5066aa2
Lucas Horner <lucas.horner@gmail.com>
2018-02-24 14:19:28 -0600

    added schema and resource model changes

--- app/models/resources_model.js
+++ app/models/resources_model.js
@@ -7,7 +7,7 @@ var Sequelize = require("sequelize");
 var sequelize = require("../config/connection.js");
 
 // Creates a "Book" model that matches up with DB
-var Resources_model = sequelize.define("resources_table", {
+/*var Resources_model = sequelize.define("resources_table", {
   resource: {
     type: Sequelize.STRING
   },
@@ -26,7 +26,79 @@ var Resources_model = sequelize.define("resources_table", {
 });
 
 // Syncs with DB
-Resources_model.sync();
+Resources_model.sync(); 
+*/
+
+
+//Creat a "business_category" model that matches up with DB
+var category_model = sequelize.define("business_category", {
+  username: {
+    type: Sequelize.STRING
+  },
+  business_name: {
+    type: Sequelize.STRING
+  },
+  business_category: {
+    type: Sequelize.STRING
+  },
+  city_dept: {
+    type: Sequelize.STRING
+  },
+  sub_category1: {
+    type: Sequelize.STRING
+  },
+  /*sub_category2: {
+    type: Sequelize.STRING
+  },
+  sub_category3: {
+    type: Sequelize.STRING
+  },
+  sub_category4: {
+    type: Sequelize.STRING
+  },*/
+  ethnicity: {
+    type: Sequelize.STRING
+  },
+  ethnicity2: {
+    type: Sequelize.STRING
+    defaultValue: null
+  },
+  /*ethnicity3: {
+    type: Sequelize.STRING
+  }*/
+  street_address: {
+    type: Sequelize.STRING
+  },
+  business_city: {
+    type: Sequelize.STRING
+  },
+  business_state: {
+    type: Sequelize.STRING
+  },
+  zip_code: {
+    type: Sequelize.STRING
+  },
+  phone_number: {
+    type: Sequelize.INTEGER
+  },
+  business_email: {
+    type: Sequelize.STRING
+  },
+  main_contact: {
+    type: Sequelize.STRING
+  },
+  contact_title: {
+    type: Sequelize.STRING
+  },
+  contact_phone: {
+    type: Sequelize.STRING
+  },
+  contact_email: {
+    type: Sequelize.STRING
+  }
+})
+
+category_model.sync();
 
 // Makes the Book Model available for other files (will also create a table)
 module.exports = Resources_model;
--- schema.sql
+++ schema.sql
@@ -1,2 +1,32 @@
 DROP DATABASE IF EXISTS `aarc_db`;
 CREATE DATABASE `aarc_db`;
+
+USE aarc_db
+
+CREATE TABLE business_category (
+    id int NOT NULL AUTO_INCREMENT,
+    username VARCHAR(50) NOT NULL,
+    business_name VARCHAR(50) NOT NULL,
+    business_category VARCHAR(50) NOT NULL,
+    city_dept VARCHAR(50) NOT NULL,
+    sub_category1 VARCHAR(50) NOT NULL,
+    --sub_category2 VARCHAR(50),
+    --sub_category3 VARCHAR(50),
+    --sub_category4 VARCHAR(50),
+    ethnicity VARCHAR(50) NOT NULL,
+    ethnicity2 VARCHAR(50),
+    --ethnicity3 VARCHAR(50),
+    street_address VARCHAR(100) NOT NULL,
+    business_city VARCHAR(50) NOT NULL,
+    business_state VARCHAR(50) NOT NULL,
+    zip_code VARCHAR(50) NOT NULL,
+    phone_number VARCHAR(50) NOT NULL,
+    business_email VARCHAR(50) NOT NULL,
+    username VARCHAR(50) NOT NULL,
+    business_name VARCHAR(50) NOT NULL,
+    main_contact VARCHAR(50) NOT NULL,
+    contact_title VARCHAR(50) NOT NULL,
+    contact_phone VARCHAR(50) NOT NULL,
+    contact_email VARCHAR(50) NOT NULL,
+    PRIMARY KEY (id)
+ )
