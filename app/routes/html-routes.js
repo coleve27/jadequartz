937b9603c3d16a2ef0301ec120fb71466d06d11c
Giovanni Pardo <giovanni_pardo@utexas.edu>
2018-04-18 23:34:24 -0500

    changed templating to handlebars

--- app/routes/html-routes.js
+++ app/routes/html-routes.js
@@ -16,26 +16,30 @@ module.exports = function(app) {
   // Each of the below routes just handles the HTML page that the user gets sent to.
 
   // index route loads view.html
-  // app.get("/", function(req, res) {
-  //   res.sendFile(path.join(__dirname, "../public/view.html"));
-  // });
+  app.get("/", function(req, res) {
+    res.render('index');
+  });
 
-  // // add route loads the add.html page, where users can enter new books to the db
-  // app.get("/add", function(req, res) {
-  //   res.sendFile(path.join(__dirname, "../public/add.html"));
-  // });
+  // add route loads the a dd.html page, where users can enter new books to the db
+  app.get("/add", function(req, res) {
+    res.render('add');
+  });
 
 
 
-  // // all route loads the all.html page, where all books in the db are displayed
-  // app.get("/all", function(req, res) {
-  //   res.sendFile(path.join(__dirname, "../public/all.html"));
-  // });
+  // all route loads the all.html page, where all books in the db are displayed
+  app.get("/all", function(req, res) {
+    res.render('all');
+  });
 
   app.get('/logout', function(req, res){
-    res.redirect('https://jdqtz.auth0.com/v2/logout?returnTo=http%3A%2F%2Flocalhost%3A8080&client_id=C3uimLj64ZbCZ3XxjiQ5T-mRt92h5u12');
+    res.redirect('https://jdqtz.auth0.com/v2/logout?returnTo=http%3A%2F%2Flocalhost%3A8080%2Floggedout&client_id=C3uimLj64ZbCZ3XxjiQ5T-mRt92h5u12');
     
   })
+
+  app.get("/loggedout", function(req, res) {
+    res.render('logout');
+  });
 
   // short route loads the short.html page, where short books in the db are displayed
   // app.get("/short", function(req, res) {
